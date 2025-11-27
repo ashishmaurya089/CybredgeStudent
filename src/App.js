eaef007057466fd16ab8970c5141d787ee413489
vishalmaurya07-hub <vishal345maurya@gmail.com>
2025-11-27 18:33:57 +0530

    text

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Portfolio from './Portfolio/Portfolio';
+import All from './Portfolio/Navigation/All'
+import Application from './Portfolio/Navigation/Application'
+import Product from './Portfolio/Navigation/Product'
+import Branding from './Portfolio/Navigation/Branding'
+import Books from './Portfolio/Navigation/Book'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+       <BrowserRouter>
+       <Portfolio />
+        <Routes>
+          <Route path="/" element={<All />} />
+          <Route path="/application" element={<Application />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/branding" element={<Branding />} />
+          <Route path="/books" element={<Books />} />
+        </Routes>
+       
+     </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/Portfolio/Portfolio.js
@@ -0,0 +1,59 @@
+
+import { NavLink } from "react-router-dom";
+
+
+function Portfolio() {
+    return (
+        <>
+            <section className="py-16 px-6 text-center mx-auto" style={{marginLeft: '100px', marginRight: '100px', marginTop: '10px', marginBottom: '10px'}}>
+                <h1 className="text-5xl font-bold text-gray-900 mb-4">PORTFOLIO</h1>
+                <p className="text-gray-600 text-lg mb-12 max-w-2xl mx-auto">
+                    Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit
+                </p>
+
+                <ul className="flex flex-wrap justify-center gap-2">
+                    <li>
+                        <NavLink to="/" className={({ isActive }) => `px-6 py-3 text-gray-700 rounded-lg transition font-bold text-lg ${isActive ? 'text-red-600' : 'hover:text-red-600'}`}>All</NavLink>
+                    </li>
+
+                    <li>
+                        <NavLink to="application" className={({ isActive }) => `px-6 py-3 text-gray-700 rounded-lg transition font-bold text-lg ${isActive ? 'text-red-600' : 'hover:text-red-600'}`}>App</NavLink>
+                    </li>
+
+                    <li>
+                        <NavLink to="/product" className={({ isActive }) => `px-6 py-3 text-gray-700 rounded-lg transition font-bold text-lg ${isActive ? 'text-red-600' : 'hover:text-red-600'}`}>Product</NavLink>
+                    </li>
+
+                    <li>
+                        <NavLink to="/branding" className={({ isActive }) => `px-6 py-3 text-gray-700 rounded-lg transition font-bold text-lg ${isActive ? 'text-red-600' : 'hover:text-red-600'}`}>Branding</NavLink>
+                    </li>
+
+                    <li>                                        
+                        <NavLink to="/books" className={({ isActive }) => `px-6 py-3 text-gray-700 rounded-lg transition font-bold text-lg ${isActive ? 'text-red-600' : 'hover:text-red-600'}`}>Books</NavLink>
+                    </li>
+                </ul>
+            </section>
+        </>
+    );
+}
+
+export default Portfolio;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
